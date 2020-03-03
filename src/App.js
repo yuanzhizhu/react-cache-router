cde78ef0af44483678665a0bd93f070ee63362d7
zhuzhiyuan <zhuzhiyuan@youzan.com>
2020-03-03 21:25:26 +0800

    feat: redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
-import CacheRoute from './lib/CacheRoute';
+import { HashRouter, Route, Redirect } from "react-router-dom";
+import CacheRoute from "./lib/CacheRoute";
 import PageA from "./pages/a";
 import PageB from "./pages/b";
 
 function App() {
   return (
     <HashRouter>
+      <Route path="/" exact render={() => <Redirect to="/a" />} />
       <CacheRoute path="/a" component={PageA} />
       <Route path="/b" component={PageB} />
     </HashRouter>
