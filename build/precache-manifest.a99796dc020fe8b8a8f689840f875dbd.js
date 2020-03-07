d524676d57089c4fa90f2e689b7218ee00fa2255
zhuzhiyuan <zhuzhiyuan@youzan.com>
2020-03-08 00:30:15 +0800

    feat: include build

---
+++ build/precache-manifest.a99796dc020fe8b8a8f689840f875dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d65b080e440448ccae30890595255a",
+    "url": "/buildindex.html"
+  },
+  {
+    "revision": "39dc1db2946770e44d74",
+    "url": "/buildstatic/js/2.79ed3b27.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/buildstatic/js/2.79ed3b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6b4ce1763c1ea2b5fa",
+    "url": "/buildstatic/js/main.a04e3a3d.chunk.js"
+  },
+  {
+    "revision": "85c2f2fcbe8245b19d9a",
+    "url": "/buildstatic/js/runtime-main.4acd281b.js"
+  }
+]);
